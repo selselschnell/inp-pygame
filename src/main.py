c0b21f0924dc59b9afcfcc1c24c7f56afe900792
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-03-16 14:35:56 +0100

    Added Graphics.

--- src/main.py
+++ src/main.py
@@ -2,22 +2,42 @@ import pygame
 
 import sys
 
+class Spritesheet:
+    def __init__(self, file):
+        self.sheet = pygame.image.load(file).convert()
+
+    def get_sprite(self, x, y, width, height):
+        sprite = pygame.Surface([width, height])
+        sprite.blit(self.sheet, (0, 0), (x, y, width, height))
+        sprite.set_colorkey(Config.WHITE)
+        return sprite
+
+
+
+class Config:
+    WINDOW_WIDTH = 640
+    WINDOW_HEIGHT = 420
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    GREY = (128, 128, 128)
+    WHITE = (255, 255, 255)
+    FPS = 30
+    TILE_SIZE = 32
+    MAX_GRAVITY = -3
+
+
 
 class BaseSprite(pygame.sprite.Sprite):
-    def __init__(self, game, x, y, x_pos=0, y_pos=0, x_shift=0, y_shift=0,
-            x_scale=1, y_scale=1, layer=0, groups=None, spritesheet=None):
+    def __init__(self, game, x, y, x_pos=0, y_pos=0, width=Config.TILE_SIZE, height=Config.TILE_SIZE, layer=0, groups=None, spritesheet=None):
         self._layer = layer
         groups = (game.all_sprites, ) if groups == None else (game.all_sprites, groups)
         super().__init__(groups)
         self.game = game
-
-        x = x * Config.TILE_SIZE
-        y = y * Config.TILE_SIZE
-        self.width = Config.TILE_SIZE * x_scale
-        self.height = Config.TILE_SIZE * y_scale
-
-        self.x_pos = x_pos * Config.TILE_SIZE + x_shift
-        self.y_pos = y_pos * Config.TILE_SIZE + y_shift
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.width = width
+        self.height = height
 
         if spritesheet == None:
             self.image = pygame.Surface([self.width, self.height])
@@ -31,18 +51,21 @@ class BaseSprite(pygame.sprite.Sprite):
                 self.height
             )
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = x * Config.TILE_SIZE
+        self.rect.y = y * Config.TILE_SIZE
 
 
 class PlayerSprite(BaseSprite):
-    def __init__(self, game, x, y):
-        super().__init__(game, x, y, groups=game.players, layer=1)
+    def __init__(self, game, x, y, **kwargs):
+        img_data = {
+            'spritesheet': Spritesheet("res/player.png"),
+        }
+        super().__init__(game, x, y, groups=game.players, layer=1, **img_data, **kwargs)
         self.y_velocity = Config.MAX_GRAVITY
         self.speed = 5
         self.standing = False
         self.color = Config.RED
-        self.image.fill(self.color)
+        # self.image.fill(self.color)
         
 
     def update(self):
@@ -79,18 +102,6 @@ class GroundSprite(BaseSprite):
         self.image.fill(Config.GREEN)
 
 
-class Config:
-    WINDOW_WIDTH = 640
-    WINDOW_HEIGHT = 420
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    GREY = (128, 128, 128)
-    FPS = 30
-    TILE_SIZE = 32
-    MAX_GRAVITY = -3
-
-
 class Game:
     def __init__(self):
         pygame.init()
@@ -98,6 +109,7 @@ class Game:
         self.font = pygame.font.Font(None, 30)
         self.screen = pygame.display.set_mode( (Config.WINDOW_WIDTH, Config.WINDOW_HEIGHT) ) 
         self.clock = pygame.time.Clock()
+        self.bg = pygame.image.load("res/bg-small.png")
 
     def new(self):
         self.playing = True
@@ -108,7 +120,7 @@ class Game:
 
         self.player = PlayerSprite(self, 10, 10)
         for i in range(20):
-            GroundSprite(self, i, 12)
+            GroundSprite(self, i , 12)
         for i in range(5, 10):
             GroundSprite(self, i, 9)
 
@@ -121,7 +133,7 @@ class Game:
         self.all_sprites.update()
 
     def draw(self):
-        self.screen.fill(Config.BLACK)
+        self.screen.blit(self.bg, (0, 0))
         self.all_sprites.draw(self.screen)
         pygame.display.update()
 
