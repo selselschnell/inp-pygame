2ec55ed8c53ff5b913bfde8f020c958680d2aa59
Sophia <s.burgstaller@vtxmail.ch>
2022-03-23 09:47:12 +0100

    Hindernisse

--- src/main.py
+++ src/main.py
@@ -177,8 +177,11 @@ class PlayerSprite(BaseSprite):
 
 class GroundSprite(BaseSprite):
     def __init__(self, game, x, y):
-        super().__init__(game, x, y, groups=game.ground, layer=0)
-        self.image.fill(Config.GREEN)
+        img_data = {
+            'spritesheet': Spritesheet("res/Hand.png"),
+        }
+        super().__init__(game, x, y, groups=game.ground, layer=0,**img_data)
+        
 
 
 class Game:
